c862df425512e6456fe7fe027543d1ba87267d94
Mark Drake <markusdrake@gmail.com>
2017-05-07 19:35:50 -0400

    added partials and variables to the mix

--- tests/index.js
+++ tests/index.js
@@ -4,6 +4,9 @@ const fs = require("fs");
 
 describe("Node Simple HTML Includes", function () {
 
+	// TODO change to async code
+	// TODO don't rely on regex (?)
+
 	it("Layout", function () {
 		let content = simpleHTMLIncludes("./tests/_layout.test.html"),
 			result = fs.readFileSync("./tests/_layout.result.html", "utf8");
@@ -11,21 +14,24 @@ describe("Node Simple HTML Includes", function () {
 		assert.equal(content, result);
 	});
 
-	// it("Html.Partial", function () {
-	// 	let result = simpleHTMLIncludes("./_html.partial.test.html");
+	it("Html.Partial", function (callback) {
+		let result = simpleHTMLIncludes("./tests/_html.partial.test.html");
 
-	// 	fs.readFile("./_html.partial.result.html", function (err, data) {
-	// 		assert.equal(data, result);
-	// 	});
-	// });
+		fs.readFile("./tests/_html.partial.result.html", function (err, data) {
+			assert.equal(data, result);
+			callback();
+		});
+	});
 
-	// it("Variable", function () {
-	// 	let result = simpleHTMLIncludes("./_variable.test.html");
-	// 	//fs.readFile("./variable.html", function (err, data) {});
+	it("Variable", function () {
+		let result = simpleHTMLIncludes("./tests/_variable.test.html", {
+			variable: "Hello World"
+		});
+		//fs.readFile("./variable.html", function (err, data) {});
 
-	// 	fs.readFile("./_variable.result.html", function (err, data) {
-	// 		assert.equal(data, result);
-	// 	});
-	// });
+		fs.readFile("./_variable.result.html", function (err, data) {
+			assert.equal(data, result);
+		});
+	});
 
 });

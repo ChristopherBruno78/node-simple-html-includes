6b471577412c0920c8c9a8adeb7b3d90d0c6fcc2
Mark Drake <markusdrake@gmail.com>
2018-04-26 21:40:27 -0400

    added test for multiple html.partials

--- tests/index.js
+++ tests/index.js
@@ -7,14 +7,14 @@ describe("Node Simple HTML Includes", function () {
 	// TODO change to async code
 	// TODO don't rely on regex (?)
 
-	it("Layout", function () {
+	it("Use a Layout along with @RenderBody()", function () {
 		let responseObject = simpleHTMLIncludes("./tests/_layout.test.html"),
 			result = fs.readFileSync("./tests/_layout.result.html", "utf8").toString();
 
 		assert.equal(responseObject.content, result);
 	});
 
-	it("Html.Partial", function (callback) {
+	it("Use @Html.Partial()", function (callback) {
 		let responseObject = simpleHTMLIncludes("./tests/_html.partial.test.html");
 
 		fs.readFile("./tests/_html.partial.result.html", function (err, data) {
@@ -23,7 +23,16 @@ describe("Node Simple HTML Includes", function () {
 		});
 	});
 
-	it("Variable", function () {
+	it("Use multiple @Html.Partials", function (callback) {
+		let responseObject = simpleHTMLIncludes("./tests/_html.partials.test.html");
+
+		fs.readFile("./tests/_html.partials.result.html", function (err, data) {
+			assert.equal(data, responseObject.content);
+			callback();
+		});
+	});
+
+	it("Use JSON as a second parameter for @Html.Partial()", function () {
 		let result = simpleHTMLIncludes("./tests/_variable.test.html", {
 			variable: "Hello World",
 			variable2: "Hello World 2"
